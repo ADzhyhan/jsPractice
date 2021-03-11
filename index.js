c2d0174a694b2845584c2c09a2fa983fdf469285
ADzhyhan <a.dzhyhan@gmail.com>
2021-03-11 16:08:16 +0200

    completion second task

--- index.js
+++ index.js
@@ -1,16 +1,29 @@
 //task1 
-let arr = [{ a: 1, b: 2}, {a: 2, c: 3}, { a: 1, b: 2 }];
+// let arr = [{ a: 1, b: 2}, {a: 2, c: 3}, { a: 1, b: 2 }];
 
-let result = arr.reduce(function (unique, value) {
-  if (
-    unique.every(function (item) {
-      return !(item.t === value.t && item.b === value.b);
-    })
-  )
-    unique.push(value);
-  return unique;
-}, []);
+// let result = arr.reduce(function (unique, value) {
+//   if (
+//     unique.every(function (item) {
+//       return !(item.t === value.t && item.b === value.b);
+//     })
+//   )
+//     unique.push(value);
+//   return unique;
+// }, []);
 
-console.log(result);
+// console.log(result);
 
 //task2 
+let array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]; 
+
+let i = -1; 
+
+function loop(arr) {
+  i++; 
+  if (i < arr.length) {
+    console.log(arr[i]);
+    loop(arr);
+  }
+}
+
+loop(array);
