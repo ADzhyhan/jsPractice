4a15beb39778c950e1711c3fb9f7b4279b86d233
ADzhyhan <a.dzhyhan@gmail.com>
2021-03-11 17:12:55 +0200

    completion third task

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
 //task1 
-// let arr = [{ a: 1, b: 2}, {a: 2, c: 3}, { a: 1, b: 2 }];
+let arr = [{ a: 1, b: 2}, {a: 2, c: 3}, { a: 1, b: 2 }];
 
-// let result = arr.reduce(function (unique, value) {
-//   if (
-//     unique.every(function (item) {
-//       return !(item.t === value.t && item.b === value.b);
-//     })
-//   )
-//     unique.push(value);
-//   return unique;
-// }, []);
+let result = arr.reduce(function (unique, value) {
+  if (
+    unique.every(function (item) {
+      return !(item.t === value.t && item.b === value.b);
+    })
+  )
+    unique.push(value);
+  return unique;
+}, []);
 
-// console.log(result);
+console.log(result);
 
 //task2 
 let array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]; 
@@ -27,3 +27,16 @@ function loop(arr) {
 }
 
 loop(array);
+
+//task3 
+function transliterate(word) {
+  const result = [];
+  const letters = ["А", "В", "С", "Н", "І", "К", "М", "а", "с", "е", "р", "і", "о", "х", "у"];
+  for (let i = 0; i < word.length; i++) {
+    if (letters.includes(word[i]) && !result.includes(word[i])) { 
+      result.push(word[i]);
+    }
+  }
+  console.log(result);
+}
+transliterate("дерево");
